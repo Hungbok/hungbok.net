666a6c1c14003101b9d8ad06aaed7d0460023ebf
Hungbok <bjminimn@naver.com>
2023-10-12 22:12:50 +0900

    css fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const setTheme = theme => document.documentElement.className = theme;
 
 $(document).ready(function(){
     $(".searchbox").append('<div class="toggle-container">'+
-                                '<button class="theme-btn light" onclick="setTheme(`light`)">☀</button>'+
-                                '<button class="theme-btn dark" onclick="setTheme(`dark`)">☾</button>'+
+                                '<button class="theme-btn light" onclick="setTheme(`light`)"><img src="https://www.hungbok.net/img/icon/light_mode.svg"></button>'+
+                                '<button class="theme-btn dark" onclick="setTheme(`dark`)"><img src="https://www.hungbok.net/img/icon/dark_mode.svg"></button>'+
                             '</div>');
 });
