8d69a4af326681341944c246508837fa6f545132
Hungbok <bjminimn@naver.com>
2023-10-12 22:21:41 +0900

    copyright

--- js/copyright.js
+++ js/copyright.js
@@ -5,6 +5,6 @@ $(document).ready(function(){
                         '</div>'+
                     '</footer>');
     $("body").append('<style>'+
-                        '@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap"); .footer { width: 100%; height: 250px; text-align: center; font-weight: bold; font-size: 16px; color: #cccccc; background-color: #000000; display: flex; align-content: center; justify-content: center; align-items: center; font-family: "Noto Sans", sans-serif; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
+                        '@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap"); .footer { width: 100%; height: 250px; text-align: center; font-weight: bold; font-size: 16px; color: var(--footer-txt); background-color: var(--footer-bg); box-shadow: 0px -2.5px 2.5px black; display: flex; align-content: center; justify-content: center; align-items: center; font-family: "Noto Sans", sans-serif; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
                     '</style>'}
 );
