1e9b867d3e332698f8050e18baa907225f352528
Hungbok <bjminimn@naver.com>
2023-10-12 23:30:36 +0900

    Update

--- js/copyright.js
+++ js/copyright.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("body").append('<footer>'+
+    $("html").append('<footer>'+
                         '<div class="footer">'+
                             '<div class="copyright"></div>'+
                         '</div>'+
                     '</footer>');
-    $("body").append('<style>'+
-                        '@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap"); .footer { width: 100%; height: 250px; text-align: center; font-weight: bold; font-size: 16px; color: var(--footer-txt); background-color: var(--footer-bg); box-shadow: 0px -2.5px 2.5px black; display: flex; align-content: center; justify-content: center; align-items: center; font-family: "Noto Sans", sans-serif; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
+    $("html").append('<style>'+
+                        '@import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap"); .footer { width: 100%; height: 250px; text-align: center; font-weight: bold; font-size: 16px; color: var(--footer-txt); background-color: var(--footer-bg); filter: drop-shadow(0px 0px 2.5px rgb(0, 0, 0)); display: flex; align-content: center; justify-content: center; align-items: center; font-family: "Noto Sans", sans-serif; } .copyright { width: 80%; } .copyright::after { content: "Copyright 2021-2023. HungBok All Rights Reserved."; }');
                     '</style>'}
 );
