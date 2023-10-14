fa5c583eca79ac99ea395aef5ba97cca63aa9189
Hungbok <bjminimn@naver.com>
2023-10-14 23:20:17 +0900

    sul

--- js/sul.js
+++ js/sul.js
@@ -1,24 +1,102 @@
-var options = {
-  valueNames: ['surl', 'url']
-};
-var hackerList = new List('hacker-list', options);
+// JSON 파일 경로
+const dataUrl = '/data/sul.json';
+const resultsPerPage = 8; // 페이지당 결과 수
 
-// Assigning references to the filter buttons
-var filterCityButton = document.getElementById("filter");
-var removeFiltersButton = document.getElementById("removeFilters");
+let currentPage = 1;
+let allData = []; // 모든 데이터를 저장하는 배열
+let filteredData = []; // 검색된 데이터를 저장하는 배열
 
-// When the filter button is clicked. The list is filtered by calling the filter function of the list object and passing in a function that accepts the list items.
-filterCityButton.addEventListener("click", function() {
-  hackerList.filter(function(item) {
-      if (item.values().city == "Edmonton") {
-          return true;
-      } else {
-          return false;
-      }
-  }); // Only items that have Edmonton as the city.
+async function loadData() {
+    try {
+        const response = await fetch(dataUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
+    }
+}
+
+async function paginateData(data, page) {
+    const startIndex = (page - 1) * resultsPerPage;
+    const endIndex = startIndex + resultsPerPage;
+    const dataToDisplay = data.slice(startIndex, endIndex);
+
+    const searchResults = document.getElementById('searchResults');
+    searchResults.innerHTML = '';
+
+    dataToDisplay.forEach(item => {
+        // 검색 결과에 추가
+        const resultItem = document.createElement('div');
+        resultItem.innerHTML = `
+        <div class="item">
+            <div class="text-content">
+                <div class="surl"><a href='https://hgbk.me/${item.title}' target='_blank'>${item.title}</a></div>
+                <div class="url"><a href='https://${item.content}' target='_blank'>${item.content}</a></div>
+            </div>
+        </div>            
+        `;
+        searchResults.appendChild(resultItem);
+    });
+}
+
+function updatePaginationButtons(data) {
+    const pagination = document.getElementById('pagination');
+    pagination.innerHTML = '';
+
+    const totalPages = Math.ceil(data.length / resultsPerPage);
+
+    for (let i = 1; i <= totalPages; i++) {
+        const pageButton = document.createElement('button');
+        pageButton.textContent = i;
+        pageButton.onclick = () => {
+            currentPage = i;
+            paginateData(data, currentPage);
+            updatePaginationButtons(data);
+        };
+        pagination.appendChild(pageButton);
+    }
+}
+
+function searchInstantly() {
+    const inputText = document.getElementById('searchInput').value;
+    const textToSearch = inputText.trim().toLowerCase();
+
+    if (!textToSearch) {
+        // 검색어가 비어 있으면 모든 데이터 표시
+        currentPage = 1;
+        paginateData(allData, currentPage);
+        updatePaginationButtons(allData);
+        return;
+    }
+
+    // 검색어와 일치하는 항목만 필터링
+    filteredData = allData.filter(item =>
+        item.title.toLowerCase().includes(textToSearch) || item.content.toLowerCase().includes(textToSearch)
+    );
+
+    currentPage = 1;
+
+    // 검색 결과 페이지를 업데이트
+    paginateData(filteredData, currentPage);
+    updatePaginationButtons(filteredData);
+}
+
+// 입력 필드 값이 변경될 때마다 실시간 검색 실행
+document.getElementById('searchInput').addEventListener('input', searchInstantly);
+
+// 초기 데이터를 로드하고 페이지를 표시하기 위해 페이지 로드 시 호출
+loadData().then(data => {
+    allData = data;
+    paginateData(allData, currentPage);
+    updatePaginationButtons(allData);
 });
 
-// When the remove filter button is clicked, the filters are removed by calling the filter function once again with no parameters.
-removeFiltersButton.addEventListener("click", function() {
-  hackerList.filter();
-});
+function searchOnEnter(event) {
+    if (event.key === 'Enter') {
+        searchData();
+    }
+}
+
+// 검색 결과 페이지를 업데이트
+paginateData(filteredData, currentPage);
+updatePaginationButtons(filteredData);
