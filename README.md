75a6348aaf8af5cad26df0917609b6bf79fb650c
Hungbok <bjminimn@naver.com>
2024-02-18 17:26:53 +0900

    Update

--- README.md
+++
@@ -1,1 +0,0 @@
-# hungbok.net
--- js/ttu.js
+++
@@ -1,9 +0,0 @@
-$.fn.copyTo = function(selector) {
-    $(selector).val($(this[0]).val().replace(/[ä@]/g, "a").replace(/[éē]/g, "e").replace(/[Ⓖ]/g, "g").replace(/[ōØ]/g, "o").replace(/[×]/g, " x ").replace(/[&]/g, " and ").replace(/[']/g, "").replace(/[`~∽☆★♪!#♯$%^*()_|+\-–−=?;:",.<>\{\}\[\]\\\/\n ]/g, "-").replace(/--------/g, "-").replace(/-------/g, "-").replace(/------/g, "-").replace(/-----/g, "-").replace(/----/g, "-").replace(/---/g, "-").replace(/--/g, "-").replace(/^-+/g, "").replace(/^-+/g, "").replace(/-+$/g, ""));
-};
-
-$(document).ready(function() {
-    $("#generate-btn").click(function() {
-        $("#textinput1_2007121003").copyTo("#textoutput2_2007121003");
-    });
-});
