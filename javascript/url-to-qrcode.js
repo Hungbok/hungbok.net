13fda987dfb0c272c0065e9cd567ffe922ff4969
Hungbok <bjminimn@naver.com>
2024-03-29 03:28:51 +0900

    Update

--- javascript/url-to-qrcode.js
+++ javascript/url-to-qrcode.js
@@ -5,7 +5,7 @@ var generateButton = document.getElementById("generateButton");
 
 // 로고 이미지를 미리 로드
 var logoImage = new Image();
-logoImage.src = '//media.hungbok.net/image/hb/hb_qrcode_icon.svg'; // 로고 이미지 파일의 경로
+logoImage.src = '//www.hungbok.net/image/hb/hb_qrcode_icon.svg'; // 로고 이미지 파일의 경로
 logoImage.onload = function() {
     // 로고 이미지 로드가 완료되면 generateQRCode 함수를 호출하여 QR 코드를 생성
     generateQRCode();
