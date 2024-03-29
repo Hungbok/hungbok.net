30d20d68f0cf93d191def836a5a354e2bfbd4b96
Hungbok <bjminimn@naver.com>
2024-03-29 19:39:39 +0900

    Update

--- javascript/youtube-thumbnail-downloader.js
+++ javascript/youtube-thumbnail-downloader.js
@@ -1,7 +1,31 @@
-function generateImages() {
-    var linkInput = document.getElementById('linkInput').value;
-    var imageContainer = document.getElementById('image');
+// YouTube URL 또는 비디오 ID에서 비디오 ID를 추출하는 함수
+function extractVideoID(urlOrId) {
+    const patterns = [
+        /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)/, // 전체 YouTube URL
+        /(?:https?:\/\/)?youtu\.be\/([a-zA-Z0-9_-]+)/, // 짧은 YouTube URL
+        /([a-zA-Z0-9_-]{11})/ // 비디오 ID 패턴
+    ];
 
+    for (let pattern of patterns) {
+        const match = urlOrId.match(pattern);
+        if (match && match[1]) {
+            return match[1];
+        }
+    }
+
+    return null; // 일치하는 패턴 없음
+}
+
+function generateImages() {
+    var input = document.getElementById('linkInput').value;
+    var videoID = extractVideoID(input);
+
+    if (!videoID) {
+        showErrorMessage('올바른 YouTube 비디오 ID나 URL을 입력해주세요.');
+        return;
+    }
+
+    var imageContainer = document.getElementById('image');
     imageContainer.innerHTML = ''; // 이미지 컨테이너 초기화
 
     // 이미지 URL 생성
@@ -21,12 +45,13 @@ function createImage(src, name) {
     var a = document.createElement('a');
     a.href = src;
     a.download = `${name}.jpg`; // 다운로드될 파일명 설정
+    a.target = "_blank"; // 새 탭에서 링크 열기
     a.style.display = 'inline-block'; // a 태그를 인라인 블록으로 설정하여 마진 적용 가능
     a.style.maxWidth = '100%';
-    
+
     var img = document.createElement('img');
     img.src = src;
-    img.style.width = '100%';
+    img.style.maxWidth = '100%';
 
     a.appendChild(img);
     
