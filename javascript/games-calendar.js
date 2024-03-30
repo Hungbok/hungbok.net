7cd327988313fc549a9fe2991421e94b2c3b1507
Hungbok <bjminimn@naver.com>
2024-03-30 21:30:33 +0900

    Update

--- javascript/games-calendar.js
+++ javascript/games-calendar.js
@@ -190,12 +190,14 @@ if (!y || !m) {
         window.addEventListener('load', function() {
             loadAsyncScripts();
 
-            $(".pc").append('<img src="https://media.hungbok.net/image/icon/display.svg">');
-            $(".ps").append('<img src="https://media.hungbok.net/image/icon/playstation.svg">');
-            $(".xb").append('<img src="https://media.hungbok.net/image/icon/xbox.svg">');
-            $(".ns").append('<img src="https://media.hungbok.net/image/icon/nintendo.svg">');
-            $(".mo").append('<img src="https://media.hungbok.net/image/icon/phone.svg">');
-            $(".cl").append('<img src="https://media.hungbok.net/image/icon/cloud.svg">');
+            $(".pc").append('<img ttt="PC" src="https://media.hungbok.net/image/icon/display.svg">');
+            $(".ps").append('<img ttt="PlayStation" src="https://media.hungbok.net/image/icon/playstation.svg">');
+            $(".xb").append('<img ttt="Xbox" src="https://media.hungbok.net/image/icon/xbox.svg">');
+            $(".ns").append('<img ttt="Nintendo" src="https://media.hungbok.net/image/icon/nintendo.svg">');
+            $(".co").append('<img ttt="Console" src="https://media.hungbok.net/image/icon/gamepad.svg">');
+            $(".ar").append('<img ttt="Arcade" src="https://media.hungbok.net/image/icon/arcade.svg">');
+            $(".mo").append('<img ttt="Mobile" src="https://media.hungbok.net/image/icon/phone.svg">');
+            $(".cl").append('<img ttt="Cloud" src="https://media.hungbok.net/image/icon/cloud.svg">');
         });
     })
     .catch(error => {
