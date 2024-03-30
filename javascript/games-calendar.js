330dbaef345e569cc413299188a4ebe38b4a1cca
Hungbok <bjminimn@naver.com>
2024-03-31 08:13:39 +0900

    Update

--- javascript/games-calendar.js
+++ javascript/games-calendar.js
@@ -186,19 +186,6 @@ if (!y || !m) {
                 "</div>"); // '#calendar-remainder'에 데이터 추가
             });
         });
-
-        $(document).ready(function() {
-            loadAsyncScripts();
-            
-            $(".pc").append('<img ttt="PC" src="https://media.hungbok.net/image/icon/display.svg">');
-            $(".ps").append('<img ttt="PlayStation" src="https://media.hungbok.net/image/icon/playstation.svg">');
-            $(".xb").append('<img ttt="Xbox" src="https://media.hungbok.net/image/icon/xbox.svg">');
-            $(".ns").append('<img ttt="Nintendo" src="https://media.hungbok.net/image/icon/nintendo.svg">');
-            $(".co").append('<img ttt="Console" src="https://media.hungbok.net/image/icon/gamepad.svg">');
-            $(".ar").append('<img ttt="Arcade" src="https://media.hungbok.net/image/icon/arcade.svg">');
-            $(".mo").append('<img ttt="Mobile" src="https://media.hungbok.net/image/icon/phone.svg">');
-            $(".cl").append('<img ttt="Cloud" src="https://media.hungbok.net/image/icon/cloud.svg">');
-        });
     })
     .catch(error => {
         console.log('Error:', error);
@@ -243,4 +230,17 @@ $('.calendar-day').on('mousedown', function() {
   
 $('.calendar-day').on('mouseup', function() {
     $(this).find('.calendar-container').trigger('mouseup');
+});
+
+$(document).ready(function() {
+    loadAsyncScripts();
+    
+    $(".pc").append('<img ttt="PC" src="https://media.hungbok.net/image/icon/display.svg">');
+    $(".ps").append('<img ttt="PlayStation" src="https://media.hungbok.net/image/icon/playstation.svg">');
+    $(".xb").append('<img ttt="Xbox" src="https://media.hungbok.net/image/icon/xbox.svg">');
+    $(".ns").append('<img ttt="Nintendo" src="https://media.hungbok.net/image/icon/nintendo.svg">');
+    $(".co").append('<img ttt="Console" src="https://media.hungbok.net/image/icon/gamepad.svg">');
+    $(".ar").append('<img ttt="Arcade" src="https://media.hungbok.net/image/icon/arcade.svg">');
+    $(".mo").append('<img ttt="Mobile" src="https://media.hungbok.net/image/icon/phone.svg">');
+    $(".cl").append('<img ttt="Cloud" src="https://media.hungbok.net/image/icon/cloud.svg">');
 });
