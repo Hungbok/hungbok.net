d1e59d630d3c104e850c7e186c049670136c992b
Hungbok <bjminimn@naver.com>
2024-04-12 02:51:00 +0900

    Update

--- javascript/music-popup.js
+++ javascript/music-popup.js
@@ -71,4 +71,77 @@ $(document).ready(function() {
         var videoID = $(this).attr("videoid");
         openVideo(e, videoID, 'https://music.line.me/webapp/embed/album/');
     });
+
+    function openTrack(e, videoID, url, width, height) {
+        e.preventDefault();
+
+        var $popup = $('<div class="music-popup" style="display: none;"></div>');
+        var $content = $('<div class="music-popup-content"></div>');
+        $content.append('<span class="music-popup-close"></span><span class="music-loading"></span><iframe class="music-iframe" src="' + url + videoID + '?width=' + width + '&height=' + height + '&isPC=true&autoPlay=false" width="' + width + '" height="' + height + '" frameborder="0" scrolling="no" allowtransparency="true" allowfullscreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture; clipboard-write"></iframe>');
+        $popup.append($content);
+        $("body").append($popup);
+
+        $popup.fadeIn();
+
+        $('body').addClass('lb-disable-scrolling');
+        $('#element').on('scroll touchmove mousewheel', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        });
+
+        $(".music-popup-close, .music-popup").click(function() {
+            var $popup = $(".music-popup");
+
+            $popup.fadeOut(400, function() {
+                $(this).remove();
+            });
+
+            $('body').removeClass('lb-disable-scrolling');
+            $('#element').off('scroll touchmove mousewheel');
+        });
+
+        $(document).keyup(function(event) {
+            if (event.keyCode == 27) {
+                var $popup = $(".music-popup");
+                
+                $popup.fadeOut(400, function() {
+                    $(this).remove();
+                });
+
+                $('body').removeClass('lb-disable-scrolling');
+                $('#element').off('scroll touchmove mousewheel');
+            }
+        });
+    }
+
+    $(document).on('click', '.spotify-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://open.spotify.com/embed/track/', '500', '152');
+    });
+
+    $(document).on('click', '.apple-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://embed.music.apple.com/album/', '500', '175');
+    });
+
+    $(document).on('click', '.amazon-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://music.amazon.com/embed/', '500', '300');
+    });
+
+    $(document).on('click', '.deezer-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://widget.deezer.com/widget/auto/track/', '500', '300');
+    });
+
+    $(document).on('click', '.naver-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://vibe.naver.com/embed/track/', '500', '154');
+    });
+
+    $(document).on('click', '.line-track', function(e) {
+        var videoID = $(this).attr("videoid");
+        openTrack(e, videoID, 'https://music.line.me/webapp/embed/track/', '500', '214');
+    });
 });
