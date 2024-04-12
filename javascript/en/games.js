e1f0e3192088dc366523e45102191e54d8fc47f4
Hungbok <bjminimn@naver.com>
2024-04-13 00:50:15 +0900

    Update

--- javascript/en/games.js
+++ javascript/en/games.js
@@ -433,8 +433,8 @@ $(document).ready(function(){
                 });
             }
             metacriticData.forEach(function(item) {
-                $(".rating-card.metacritic.rating-three-score").append('<div class="rating-card rating-two-score ' + item.class + '" ttt="' + item.ttt + '">'+
-                    '<img class="rating-platform" src="//media.hungbok.net/image/icon/' + item.img + '.svg" onerror="this.src=`//media.hungbok.net/image/hb/hb_error.svg`;" loading="lazy">'+
+                $(".rating-card.metacritic.rating-three-score").append('<div class="rating-card rating-two-score ' + item.class + '">'+
+                    '<div class="rating-platform icon-' + item.class + '" ttt="' + item.ttt + '"></div>'+
                     '<div class="rating-score">'+
                         '<div class="rating-title">메타스코어</div>'+
                         "<a href='" + item.link + "' class='rating-star' target='_blank' ttt='" + item.score + "'>"+
