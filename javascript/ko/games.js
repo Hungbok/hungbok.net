248d27b9bf3303a6e4f9862de2733a3a4b323bf5
Hungbok <bjminimn@naver.com>
2024-04-13 23:24:54 +0900

    Update

--- javascript/ko/games.js
+++ javascript/ko/games.js
@@ -1301,12 +1301,17 @@ $(document).ready(function(){
                         startProgressBar();
                     }
                 }
+                
+                var slideTimer = setInterval(function() {
+                    $carousel.trigger('next.owl.carousel');
+                }, 5000);
             
                 // 마우스 오버 시 자동 재생을 멈추고 진행 바의 애니메이션도 멈춥니다.
                 $carousel.on('mouseover', function() {
                     $carousel.trigger('stop.owl.autoplay');
                     $progressBar.css({width: $progressBar.width(), transition: 'none'});
                     isMouseOver = true;
+                    clearInterval(slideTimer);
                 });
             
                 // 마우스 아웃 시 자동 재생을 재개하고 진행 바 애니메이션을 다시 시작합니다.
@@ -1314,6 +1319,9 @@ $(document).ready(function(){
                     $carousel.trigger('play.owl.autoplay');
                     isMouseOver = false;
                     startProgressBar();
+                    slideTimer = setInterval(function() {
+                        $carousel.trigger('next.owl.carousel');
+                    }, 5000);
                 });
             });
 
