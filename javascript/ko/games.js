98a5bda6a0f5791cb2c833c9a518c23bb7958592
Hungbok <bjminimn@naver.com>
2024-04-14 00:40:57 +0900

    Update

--- javascript/ko/games.js
+++ javascript/ko/games.js
@@ -1293,6 +1293,7 @@ $(document).ready(function(){
                         checkMouseAndStartProgressBar();
                     }
                 }).on('translated.owl.carousel', function(e) {
+                    startProgressBar();
                     autoSlide();
                 });
 
