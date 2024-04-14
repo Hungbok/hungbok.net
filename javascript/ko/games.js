7f17d6f6c95e3cf6cf7d1ab582d3208cedfb1f5c
Hungbok <bjminimn@naver.com>
2024-04-14 17:00:11 +0900

    Update

--- javascript/ko/games.js
+++ javascript/ko/games.js
@@ -868,7 +868,7 @@ $(document).ready(function(){
             '<div class="discover-title">'+
                 '<div class="discover-text">최신 할인</div>'+
             '</div>'+
-            '<div class="discover-container" id="games-sale"></div>'+
+            '<div class="discover-container" id="upcomingContainer"></div>'+
             '<div class="discover-title">'+
                 '<div class="discover-text">Epic Games Store</div>'+
             '</div>'+
@@ -1364,7 +1364,7 @@ $(document).ready(function(){
                     </a>
                 `;
         
-                document.getElementById('games-sale').appendChild(div);
+                document.getElementById('upcomingContainer').appendChild(div);
         
                 startTimer();
             }
