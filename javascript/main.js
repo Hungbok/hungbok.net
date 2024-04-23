9b3fa6e2685178ec743edfa4459ed7bf886854fe
Hungbok <bjminimn@naver.com>
2024-04-23 21:11:57 +0900

    Update

--- javascript/main.js
+++ javascript/main.js
@@ -254,44 +254,32 @@ $(document).ready(function() {
     // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
-    var delta = 5; // 동작의 구현이 시작되는 위치
-    var navbarHeight = $('.site-header-background').outerHeight(); // 영향을 받을 요소를 선택
-    
-    // 스크롤시에 사용자가 스크롤했다는 것을 알림
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
     $(window).scroll(function(event) {
         didScroll = true;
     });
-    
-    // hasScrolled()를 실행하고 didScroll 상태를 재설정
     setInterval(function() {
         if (didScroll) {
             hasScrolled();
             didScroll = false;
         }
     }, 250);
-    
-    // 동작을 구현
+
     function hasScrolled() {
-        // 접근하기 쉽게 현재 스크롤의 위치를 저장한다.
         var st = $(this).scrollTop();
-    
-        // 설정한 delta 값보다 더 스크롤되었는지를 확인한다.
         if (Math.abs(lastScrollTop - st) <= delta) {
             return;
         }
-    
-        // 헤더의 높이보다 더 스크롤되었는지 확인하고 스크롤의 방향이 위인지 아래인지를 확인한다.
         if (st > lastScrollTop && st > navbarHeight) {
             // Scroll Down
-            $('.site-header-background').removeClass('nav-down').addClass('nav-up');
+            $('.site-header-background').removeClass('header-down').addClass('header-up');
         } else {
             // Scroll Up
             if (st + $(window).height() < $(document).height()) {
-                $('.site-header-background').removeClass('nav-up').addClass('nav-down');
+                $('.site-header-background').removeClass('header-up').addClass('header-down');
             }
         }
-    
-        // lastScrollTop 에 현재 스크롤위치를 지정한다.
         lastScrollTop = st;
     }
 
