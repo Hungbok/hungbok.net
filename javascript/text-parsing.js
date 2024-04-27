b4c045f913441123655734b1008dbbb79c6096b8
Hungbok <bjminimn@naver.com>
2024-04-28 08:35:16 +0900

    Update

--- javascript/text-parsing.js
+++ javascript/text-parsing.js
@@ -38,9 +38,18 @@ window.onload = function() {
     });
 };
 
-$(document).ready(function() {
-    $("#generate-btn").click(function() {
-        $("#textinput").copyTo("#textoutput");
+document.addEventListener('DOMContentLoaded', function() {
+    // 선택할 요소들의 셀렉터를 배열로 저장합니다.
+    var selectors = ['#textoutput', '.filterDetail', 'input#inputD', 'input#stringBox'];
+
+    // 각 셀렉터에 대해 반복하면서 이벤트 리스너를 추가합니다.
+    selectors.forEach(function(selector) {
+        var elements = document.querySelectorAll(selector);
+        elements.forEach(function(element) {
+            element.addEventListener('click', function() {
+                this.select();
+            });
+        });
     });
 });
 
