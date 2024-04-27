e8111272d3e64f6aeac35aa44a64fd40cbfe983e
Hungbok <bjminimn@naver.com>
2024-04-27 19:30:12 +0900

    Update

--- javascript/ko/anime.js
+++ javascript/ko/anime.js
@@ -604,7 +604,7 @@ $(document).ready(function(){
                     $(document).ready(function() {
                         $('.info-slider').owlCarousel({
                             nav: true,
-                            loop: true,
+                            loop: false,
                             dots: false,
                             merge: true,
                             autoplay: false,
